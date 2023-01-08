d7a113d528835830ccba29d828d23db5ce1cf026
Mohammad Hasan Pourghannad <mhpourghannad@gmail.com>
2023-01-08 19:21:37 +0330

    build

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainComponent from './main';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MainComponent />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './fonts.css';
-import './index.css';
-import MainComponent from './main';
-import { unregister } from './registerServiceWorker';
-ReactDOM.render(<MainComponent />, document.getElementById('root'));
-unregister();
